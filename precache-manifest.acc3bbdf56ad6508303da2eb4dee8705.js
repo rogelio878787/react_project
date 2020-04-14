72b3a4af45f663f60681a19addf81a0c8f4d33a4
Rogelio <rogeliogc345@gmail.com>
2020-04-14 14:27:52 -0500

    Updates

---
+++ precache-manifest.acc3bbdf56ad6508303da2eb4dee8705.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fc11c3581db6aaefb678ecef444294",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "ee6293c75e70b0fd6b51",
+    "url": "/react_project/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "53b8ecbb8824b75852a1",
+    "url": "/react_project/static/css/main.e92b111b.chunk.css"
+  },
+  {
+    "revision": "ee6293c75e70b0fd6b51",
+    "url": "/react_project/static/js/2.be4a35b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_project/static/js/2.be4a35b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53b8ecbb8824b75852a1",
+    "url": "/react_project/static/js/main.7f013542.chunk.js"
+  },
+  {
+    "revision": "704df8a501db01e34bc6",
+    "url": "/react_project/static/js/runtime-main.70eee6eb.js"
+  }
+]);
